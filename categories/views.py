94dd9621425e1e985dd31a3ef47c9346c471e4bb
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-10-11 23:05:06 +0200

    Design CategoryList view with get and post methods, create corresponging app and project url paths

--- categories/views.py
+++ categories/views.py
@@ -2,7 +2,29 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Category
 from .serializers import CategorySerializer
+
+
+class CategoryList(APIView):
+    """
+    Return all categories the current user 
+    has created and allow creation of new categories
+    """
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        categories = Category.objects.filter(owner=request.user)
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)  
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
