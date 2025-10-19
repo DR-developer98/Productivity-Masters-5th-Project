efdfafc22fbebc0a1c1ba507f0ba6a5e32722b37
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-10-19 22:39:56 +0200

    Add STATIC_ROOT to settings.py to allow deployment on Heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -35,6 +35,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
