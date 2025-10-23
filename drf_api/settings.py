91060e818fea6608f70418381dd9851950eaee84
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-10-23 22:45:34 +0200

    Substitute the deployed Heroku app url by os.environ.get() in allowed hosts in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,9 +65,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
     'localhost',
     '127.0.0.1',
-    'productivity-masters-8d3ed210249c.herokuapp.com',
     ]
 
 
