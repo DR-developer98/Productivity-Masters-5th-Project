f3daa9c9a1520cc8165090d261e2104a8100a666
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-10-27 19:10:02 +0100

    Create and implement CurrentUserContext in App.js to allow sharing user logged in status and data throughout application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,58 @@
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
 import Container from "react-bootstrap/Container";
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import "../src/api/axiosDefaults";
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import axios from "axios";
+import { createContext, useEffect } from "react";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>My tasks</h1>}/>
-          <Route exact path="/mycategories" render={() => <h1>My categories</h1>}/>
-          <Route exact path="/myreminders" render={() => <h1>My reminders</h1>}/>
-          <Route exact path="/signin" render={() => <SignInForm/>}/>
-          <Route exact path="/signup" render={() => <SignUpForm/>}/>
-          <Route render={() => <p>Page not found!</p>}/>
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>My tasks</h1>} />
+              <Route
+                exact
+                path="/mycategories"
+                render={() => <h1>My categories</h1>}
+              />
+              <Route
+                exact
+                path="/myreminders"
+                render={() => <h1>My reminders</h1>}
+              />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
